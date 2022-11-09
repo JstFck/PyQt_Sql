a6ede74b425ccf8119af40d7e385a6e933f4139e
JstFck <101992919+JstFck@users.noreply.github.com>
2022-11-09 19:41:30 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,33 @@ import sys
 
 
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLabel, QTextEdit
 
 
-class Window2(QWidget):
+class Open(QWidget):
     def __init__(self):
-        super(Window2, self).__init__()
-        self.setWindowTitle('Window2')
+        super(Open, self).__init__()
+
+
+class Create(QWidget):
+    def __init__(self):
+        super(Create, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Добавление базы данных')
+
+        self.label = QLabel('Введите имя базы данных', self)
+        self.move(0, 0)
+
+        self.text_edit = QTextEdit(self)
+        self.text_edit.move(0, 30)
+
+
+class Edit(QWidget):
+    def __init__(self):
+        super(Edit, self).__init__()
 
 
 class MainWindow(QMainWindow):
@@ -18,14 +38,32 @@ class MainWindow(QMainWindow):
 
     def initUI(self):
         self.setWindowTitle('main')
-        self.setGeometry(300, 300, 300, 300)
+        self.setGeometry(300, 300, 50, 130)
 
-        self.btn = QPushButton('Открыть', self)
-        self.btn.clicked.connect(self.show_window_2)
+        self.open_btn = QPushButton('Открыть', self)
+        self.open_btn.move(0, 0)
+        self.create_btn = QPushButton('Создать', self)
+        self.create_btn.move(0, 30)
+        self.edit_btn = QPushButton('Редактировать', self)
+        self.edit_btn.move(0, 60)
+        self.open_btn.clicked.connect(self.show_window_open)
+        self.create_btn.clicked.connect(self.show_window_create)
+        self.edit_btn.clicked.connect(self.show_window_edit)
 
-    def show_window_2(self):
-        self.w2 = Window2()
-        self.w2.show()
+        global db_dict
+        db_dict = {}
+
+    def show_window_open(self):
+        self.open = Open()
+        self.open.show()
+
+    def show_window_create(self):
+        self.create = Create()
+        self.create.show()
+
+    def show_window_edit(self):
+        self.edit = Edit()
+        self.edit.show()
 
 
 if __name__ == '__main__':
